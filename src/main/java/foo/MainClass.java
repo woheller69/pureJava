348c0a8a7e16ba78dddc517e6b40164160158cb8
woheller69 <wolfgang.heller@web.de>
2022-08-06 16:39:45 +0200

    read file from resources

--- src/main/java/foo/MainClass.java
+++ src/main/java/foo/MainClass.java
@@ -1,11 +1,22 @@
 package foo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ResourceBundle;
 
 public class MainClass {
     public static void main(String[] arg) {
         ResourceBundle bundle = ResourceBundle.getBundle("strings");
         System.out.println("" + bundle.getString("hello"));
-
+        InputStream inputStream = MainClass.class.getResourceAsStream("/files/test.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        try {
+            String line = reader.readLine();
+            System.out.println(line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
